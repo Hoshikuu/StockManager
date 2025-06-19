a33365ba18563f62fc47f5aea23bd88744cbc74e
Hoshiku <145494860+Hoshikuu@users.noreply.github.com>
2025-06-20 00:34:51 +0200

    Added Stuff

--- web/models.py
+++ web/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    # Imagen
+    # Color
+    # Genero
+    tallas = models.CharField(max_length=12)
+    
+
+    def __str__(self):
+        return self.nombre
--- web/views.py
+++ web/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
-def hola(request):
-    return HttpResponse("Hola Mundo")
+from .models import Producto
+
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'inventario/lista.html', {'productos': productos})
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        tallas = [request.POST.get('35'), request.POST.get('36'), request.POST.get('37'), request.POST.get('38'), request.POST.get('39'), request.POST.get('40'), request.POST.get('41'), request.POST.get('42'), request.POST.get('43'), request.POST.get('44'), request.POST.get('45'), request.POST.get('46'),]
+        if nombre and tallas:
+            Producto.objects.create(nombre=nombre, tallas=tallas)
+            return redirect('lista')
+    return render(request, 'inventario/agregar.html')
